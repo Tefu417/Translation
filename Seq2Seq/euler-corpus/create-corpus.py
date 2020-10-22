6863dcf09ce3890cb8fd28a803ada6300a6a95b6
Emi <sakura.math.sashimo@gmail.com>
2020-10-22 20:07:08 +0900

    corpus

--- Seq2Seq/euler-corpus/create-corpus.py
+++ Seq2Seq/euler-corpus/create-corpus.py
@@ -1,9 +1,12 @@
 # 2~20.txt →　Code(tab)## Japanese
 # それぞれの行を <SOS>Code<tab>Japanese<EOS> という形にする
 
+# train-euler-corpus.txt から test-euler-corpus.txt へ 10件分手動で移動
+# KeyError を防ぐ為、train-euler-corpus に単語があることを確認しながら移動
+
 import glob
 
-with open('euler-corpus.txt', 'a') as newfile :
+with open('Seq2Seq/euler-corpus.txt', 'a') as newfile :
 
     for f in glob.glob('Seq2Seq/euler-corpus/*.txt') :
         with open(f, 'r') as oldfile :
