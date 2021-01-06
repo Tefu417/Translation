e32ce61970717e53007679915dc919f03ec53ff2
Emi <sakura.math.sashimo@gmail.com>
2021-01-07 02:06:08 +0900

    add aoj_data

---
+++ aoj_data_txt/create_corpus.py
@@ -0,0 +1,13 @@
+# ja: 1773~
+
+import linecache
+# linecache.getline(filename, 整数) 整数で指定した行を読み込む
+
+cnt = 0
+
+with open('aoj_train_corpus.txt', 'a') as f :
+    for i in range(1772) :
+        py = linecache.getline('aoj_data_txt/aoj_train.txt', i + 1)
+        ja = linecache.getline('aoj_data_txt/aoj_train.txt', i + 1773)
+        f.write('<SOS>' + py[:-1] + '<tab>' + ja[:-1] + '<EOS>')
+        f.write('\n')
