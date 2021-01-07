60f41c9fd5af69509a1b710c1da041a039bc34e2
Emi <sakura.math.sashimo@gmail.com>
2021-01-08 00:52:25 +0900

    aoj_data create corpus

--- aoj_data/marge.py
+++ aoj_data/marge.py
@@ -1,5 +1,5 @@
-with open('aoj_data_txt/aoj_train.txt', 'a') as f :
-    with open('aoj_data_txt/train_py.txt', 'r') as py, open('aoj_data_txt/train_ja.txt', 'r') as ja :
+with open('aoj_data_txt/aoj_valid.txt', 'a') as f :
+    with open('aoj_data_txt/valid_py.txt', 'r') as py, open('aoj_data_txt/valid_ja.txt', 'r') as ja :
         for line in py :
             f.write(line)
         for line in ja :
